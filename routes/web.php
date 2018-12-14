a163785136f99c5da23ad6c9956650bd17b0d127
natthaphonkhamfong <tam3393@hotmail.co.th>
2018-12-14 21:53:03 +0700

    add admin page

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,25 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/admin', 'AdminIndexController@index');
+Route::get('/admin/hq', 'AdminHQController@index');
+Route::get('/admin/hq/add', 'AdminHQController@add_show');
+Route::post('/admin/hq/add', 'AdminHQController@add');
+Route::get('/admin/hq/show', 'AdminHQController@hq_show');
+Route::match(['get', 'post'], '/admin/hq/edit', 'AdminHQController@edit');
+Route::match(['get', 'post'], '/admin/hq/edit/send', 'AdminHQController@edit_send');
+Route::post('/admin/hq/delete', 'AdminHQController@delete');
+Route::get('/admin/branch', 'AdminBranchController@index');
 
-Route::get('/admin', function(){
-    return view('admin.home.index',[
-        'title' => 'Admin Dashboard',
-        'content_header'=> 'Admin Dashboard',
-        'banks' => 'CPEBank',
-        'name' => 'OK',
 
-    ]);
-});
+Route::get('/admin/branch/show', 'AdminBranchController@show');
+Route::match(['get', 'post'], '/admin/branch/add', 'AdminBranchController@add');
+Route::match(['get', 'post'], '/admin/branch/edit', 'AdminBranchController@edit');
+Route::match(['get', 'post'], '/admin/branch/edit/send', 'AdminBranchController@edit_send');
+Route::post('/admin/branch/delete', 'AdminBranchController@delete');
+
+
+Route::get('/admin/4', 'AdminIndexController@test4');
+Route::get('/admin/5', 'AdminIndexController@test5');
+Route::get('/admin/6', 'AdminIndexController@test6');
+Route::get('/admin/7', 'AdminIndexController@test7');
