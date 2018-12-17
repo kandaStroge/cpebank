66cdf3f64546631c51c4f864e0a17837b5af98af
lalitaban <lalita.bandasak@gmail.com>
2018-12-17 16:26:44 +0700

    add customer layout

---
+++ resources/views/customer/layout.blade.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>{{$title}}</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="HostSpace template project">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="/styles/bootstrap-4.1.2/bootstrap.min.css">
+<link href="/plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="/plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="/plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+<link rel="stylesheet" type="text/css" href="/plugins/OwlCarousel2-2.2.1/animate.css">
+<link href="/plugins/colorbox/colorbox.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="/styles/about.css">
+<link rel="stylesheet" type="text/css" href="/styles/about_responsive.css">
+</head>
+<body>
+
+<div class="super_container">
+	
+	<!-- Header -->
+
+	<header class="header trans_400">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<div class="header_content d-flex flex-row align-items-center justify-content-start trans_400">
+						<div class="logo"><a href="#">CPE<span>Bank</span></a></div>
+						<nav class="main_nav ml-auto mr-auto">
+							<ul class="d-flex flex-row align-items-center justify-content-start">
+								<li><a href="index.html"></a></li>
+								<li class="active"><a href="showbalance.html">show balance</a></li>
+								<li><a href="โปรโมชั่น.html">โปรโมชั่น/แนะนำบริการ</a></li>
+								<li><a href="showค้างจ่าย.html">show ค้างจ่าย</a></li>
+								<li><a href="คำร้องขอ.html">ส่งปัญหา/คำร้องขอ</a></li>
+							</ul>
+						</nav>
+						<div class="log_reg">
+								<div class="log_reg_content d-flex flex-row align-items-center justify-content-start">
+									<div class="login log_reg_text"><a href="index.html">Logout</a></div>
+									
+								</div>
+							</div>
+						
+						<div class="hamburger ml-auto"><i class="fa fa-bars" aria-hidden="true"></i></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<!-- Menu -->
+	
+	<div class="menu_overlay trans_400"></div>
+	<div class="menu trans_400">
+		<div class="menu_close_container"><div class="menu_close"><div></div><div></div></div></div>
+		
+		<nav class="menu_nav">
+			<ul>
+				<li><a href="index.html">home</a></li>
+				<li><a href="showbalance.html">show balance</a></li>
+				<li><a href="โปรโมชั่น.html">โปรโมชั่น/แนะนำบริการ</a></li>
+				<li><a href="showค้างจ่าย.html">show ค้างจ่าย</a></li>
+				<li><a href="คำร้องขอ.html">ส่งปัญหา/คำร้องขอ</a></li>
+			</ul></a></li>
+			</ul>
+		</nav>
+	</div>
+
+	<!-- Home -->
+
+	<div class="home">
+		<div class="home_background"></div>
+		<div class="background_image background_city" style="background-image:url(images/city_2.png)"></div>
+		<div class="cloud cloud_1"><img src="images/cloud.png" alt=""></div>
+		<div class="cloud cloud_2"><img src="images/cloud.png" alt=""></div>
+		<div class="cloud cloud_3"><img src="images/cloud_full_2.png" alt=""></div>
+		<div class="cloud cloud_4"><img src="images/cloud.png" alt=""></div>
+		<div class="home_container">
+			<div class="container">
+				<div class="row">
+					<div class="col">
+						<div class="home_content">
+							<div class="home_title">{{$title}}</div>
+							<div class="breadcrumbs">
+								<ul class="d-flex flex-row align-items-center justify-content-start">
+									<li><a href="index.html">Home</a></li>
+									
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container">
+			
+
+		
+
+	@yield("content");
+
+	
+
+	
+
+	
+
+
+
+	
+</div>
+
+<script src="/js/jquery-3.2.1.min.js"></script>
+<script src="/styles/bootstrap-4.1.2/popper.js"></script>
+<script src="/styles/bootstrap-4.1.2/bootstrap.min.js"></script>
+<script src="/plugins/greensock/TweenMax.min.js"></script>
+<script src="/plugins/greensock/TimelineMax.min.js"></script>
+<script src="/plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="/plugins/greensock/animation.gsap.min.js"></script>
+<script src="/plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="/plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="/plugins/easing/easing.js"></script>
+<script src="/plugins/progressbar/progressbar.min.js"></script>
+<script src="/plugins/parallax-js-master/parallax.min.js"></script>
+<script src="/plugins/colorbox/jquery.colorbox-min.js"></script>
+<script src="/js/about.js"></script>
+</body>
+</html>
---
+++ resources/views/customer/login.blade.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Services</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="HostSpace template project">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="/styles/bootstrap-4.1.2/bootstrap.min.css">
+<link href="/plugins/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="/plugins/OwlCarousel2-2.2.1/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="/plugins/OwlCarousel2-2.2.1/owl.theme.default.css">
+<link rel="stylesheet" type="text/css" href="/plugins/OwlCarousel2-2.2.1/animate.css">
+<link rel="stylesheet" type="text/css" href="/styles/services.css">
+<link rel="stylesheet" type="text/css" href="/styles/services_responsive.css">
+</head>
+<body>
+
+<div class="super_container">
+	
+	<!-- Header -->
+
+	<header class="header trans_400">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<!-- Menu -->
+	
+	<div class="menu_overlay trans_400"></div>
+
+
+	<!-- Home -->
+
+	<div class="home">
+		<div class="home_background"></div>
+		<div class="background_image background_city" style="background-image:url(images/city_3.png)"></div>
+		<div class="cloud cloud_1"><img src="images/cloud.png" alt=""></div>
+		<div class="cloud cloud_2"><img src="images/cloud.png" alt=""></div>
+		<div class="cloud cloud_3"><img src="images/cloud_full_2.png" alt=""></div>
+		<div class="cloud cloud_4"><img src="images/cloud.png" alt=""></div>
+		<div class="home_container">
+			<div class="container">
+				<div class="row">
+					<div class="col">
+						<div class="home_content">
+							<div class="home_title">Login</div>
+							<div class="breadcrumbs">
+								<ul class="d-flex flex-row align-items-center justify-content-start">
+									<li><a href="index.html">Home</a></li>
+									
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+
+			
+		</div>
+	</div>
+
+	<div class="container">
+			<div class="row">
+				
+				<div class="col-lg-6 footer_col magic_fade_in" style="visibility: inherit; opacity: 1;">
+					<div class="footer_about">
+						
+						
+						
+						<div class="contact_container">
+							<form action="#" id="contact_form" class="contact_form">
+								<div class="row">
+									
+									<div class="col-md-6">
+										<input type="email" class="contact_input" placeholder="Your e-mail" required="required">
+									</div>
+								</div>
+									<div class="row">
+									<div class="col-md-6">
+											<input type="password" class="contact_input" placeholder="Password" required="required">
+										</div>
+								</div>
+								</div>
+								
+								<button class="contact_button">Send</button>
+							</form>
+						</div>
+					</div>
+				</div>
+
+				
+
+			</div>
+		</div>
+
+	
+
+
+
+</div>
+
+<script src="/js/jquery-3.2.1.min.js"></script>
+<script src="/styles/bootstrap-4.1.2/popper.js"></script>
+<script src="/styles/bootstrap-4.1.2/bootstrap.min.js"></script>
+<script src="/plugins/greensock/TweenMax.min.js"></script>
+<script src="/plugins/greensock/TimelineMax.min.js"></script>
+<script src="/plugins/scrollmagic/ScrollMagic.min.js"></script>
+<script src="/plugins/greensock/animation.gsap.min.js"></script>
+<script src="/plugins/greensock/ScrollToPlugin.min.js"></script>
+<script src="/plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="/plugins/easing/easing.js"></script>
+<script src="/plugins/progressbar/progressbar.min.js"></script>
+<script src="/plugins/parallax-js-master/parallax.min.js"></script>
+<script src="/js/services.js"></script>
+</body>
+</html>
